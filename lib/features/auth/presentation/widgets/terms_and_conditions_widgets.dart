ac75ac6ae428c9b6fcc5a159cd4149cbeac69d7d
QusailahJalal <jalalqusailah@gmail.com>
2024-02-13 01:41:51 +0300

    add firebase to the project

--- lib/features/auth/presentation/widgets/terms_and_conditions_widgets.dart
+++ lib/features/auth/presentation/widgets/terms_and_conditions_widgets.dart
@@ -29,3 +29,4 @@ class TermsAndConditionsWidgets extends StatelessWidget {
     );
   }
 }
+  
--- lib/main.dart
+++ lib/main.dart
@@ -2,20 +2,27 @@ import 'package:dalel_app/core/database/cache/cache_helper.dart';
 import 'package:dalel_app/core/routes/app_router.dart';
 import 'package:dalel_app/core/services/service_locator.dart';
 import 'package:dalel_app/core/utils/app_colors.dart';
+import 'package:dalel_app/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:device_preview/device_preview.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   setupServiceLocator();
 
   await getIt<CacheHelper>().init();
   runApp(
-    DevicePreview(
-      enabled: !kReleaseMode,
-      builder: (context) => const Dalel(), // Wrap your app
-    ),
+    // DevicePreview(
+    //   enabled: !kReleaseMode,
+    //   builder: (context) => const Dalel(),
+    //    // Wrap your app
+    // ),
+    const Dalel(),
   );
 }
 
